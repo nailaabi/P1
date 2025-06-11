50381bccfc2d3832195d8a8ac0d58c390def3126
nailaabi <pallares.nailaabigail@iesconcepcion.edu.ar>
2025-06-11 19:27:04 -0300

    Foto1

---
+++ index.js
@@ -0,0 +1,17 @@
+//1 ingresa tu numero favorito , cual es el doble de tu numero favorito 
+
+const prompt = require( 'prompt-sync') ();
+
+const num1=Number ( prompt( 'Ingresa tu numero favorito: '));
+
+const num2= Number ( prompt ('Cual es el doble de tu numero favorito??')) 
+
+const doble = num1 * 2
+
+
+if(num2 == doble){
+    console.log ('Exacto el doble de tu numero es:', doble)
+} else { 
+    console.log ('Ese no es el doble de tu numero favorito. ');
+}
+   
